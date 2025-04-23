9515387e9b802412745fdfdc2fe402caa4525d8b
Sriram <115489336+sriram84502@users.noreply.github.com>
2025-04-23 21:38:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      useNewUrlParser: true
     });
     console.log('MongoDB Connected');
   } catch (err) {
